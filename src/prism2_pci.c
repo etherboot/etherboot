9500b2aec7812d7a89138270c6edf54ce306ef9e
Michael Brown <mcb30@etherboot.org>
2002-04-10 12:47:11 +0000

    Working drivers for Prism2 wireless cards Have been tested with:   Netgear MA301/MA401 (Prism2, PLX)   ActionTec HWP01170 (Prism2.5, PCI)   D-Link DWL-520 (Prism2.5, PCI)

---
+++ src/prism2_pci.c
@@ -0,0 +1,18 @@
+/**************************************************************************
+Etherboot -  BOOTP/TFTP Bootstrap Program
+Prism2 NIC driver for Etherboot
+Wrapper for prism2_pci
+
+Written by Michael Brown of Fen Systems Ltd
+$Id$
+***************************************************************************/
+
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at
+ * your option) any later version.
+ */
+
+#define WLAN_HOSTIF WLAN_PCI
+#include "prism2.c"
