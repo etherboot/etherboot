93ef5c39d7f597b2fafb242551890d766acb9dfe
Timothy Legge <tlegge@rogers.com>
2002-04-27 11:42:32 +0000

    Added support for the 3c515 driver

--- src/cards.h
+++ src/cards.h
@@ -55,6 +55,11 @@ extern struct nic	*t529_probe(struct nic *, unsigned short *
 	PCI_ARG(struct pci_device *));
 #endif
 
+#ifdef  INCLUDE_3C515
+extern struct nic	*t515_probe(struct nic *, unsigned short *
+		PCI_ARG(struct pci_device *));
+#endif
+
 #ifdef	INCLUDE_3C595
 extern struct nic	*t595_probe(struct nic *, unsigned short *
 	PCI_ARG(struct pci_device *));
