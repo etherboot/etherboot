a2c581edefdfb50be9e1c5ed95acc5f5fbc4e1ea
Eric W. Biederman <ebiederman@lnxi.com>
2002-12-12 04:16:24 +0000

    First step of fixing the build process to be ia64 friendly

--- src/genrules.pl
+++ src/genrules.pl
@@ -8,8 +8,9 @@
 #
 
 use strict;
+use File::Basename;
 
-use vars qw($curfam %drivers %pcient %isaent %buildent);
+use vars qw($curfam %drivers %pcient %isaent %buildent $arch);
 
 sub gendep ($) {
 	my ($driver) = @_;
@@ -22,11 +23,21 @@ sub gendep ($) {
 	while ($#new_dep >= 0) {
 		my @collect_dep = ();
 		foreach my $source (@new_dep) {
-# Warn about failure to open, then skip, rather than soldiering on with the read
-			unless (open(INFILE, "$source")) {
-				print STDERR "$source: $! (shouldn't happen)\n";
+			my $inc;
+			$inc = "arch/$arch/include/$source" unless ! -e "arch/$arch/include/$source";
+			$inc = "include/$source" unless ! -e "include/$source";
+			$inc = dirname($driver) . "/$source" unless ! -e dirname($driver) . "/$source";
+			$inc = $source unless $source ne "$driver.c";
+			unless (defined($inc)) {
+				print STDERR "$source not found (shouldn't happen)\n";
 				next;
-			}
+			};
+# Warn about failure to open, then skip, rather than soldiering on with the read
+			unless (open(INFILE, "$inc")) {
+				print STDERR "$inc: $! (shouldn't happen)\n";
+				next;
+			};
+			$driver_dep{"$source"} = $inc;
 			while (<INFILE>) {
 				chomp($_);
 # This code is not very smart: no C comments or CPP conditionals processing is
@@ -46,7 +57,7 @@ sub gendep ($) {
 		}
 		@new_dep = @collect_dep;
 	}
-	return (join(' ', sort keys %driver_dep));
+	return (join(' ', sort values %driver_dep));
 }
 
 sub addfam ($) {
@@ -71,7 +82,7 @@ sub addrom ($) {
 	} else {
 		# We store a list of PCI IDs and comments for each PCI target
 		push(@{$pcient{$curfam}}, [$rom, $ids, $comment]);
-		$buildent{$curfam} = 1;
+		$buildent{basename($curfam)} = 1;
 	}
 }
 
@@ -82,8 +93,8 @@ sub isaonly ($) {
 	return ($#$aref < 0);
 }
 
-$#ARGV >= 0 or die "Usage: $0 configfile\n";
-open(STDIN, $ARGV[0]) or die "$ARGV[0]: $!\n";
+$#ARGV >= 1 or die "Usage: $0 arch configfile\n";
+$arch = shift(@ARGV);
 
 $curfam = '';
 while(<>) {
@@ -111,20 +122,20 @@ print "# Driver object files and ROM image files\n";
 print "IMG3S2\t:=\n";
 print "DOBJS\t:=\n";
 foreach my $img (sort keys %buildent) {
-	print "DOBJS\t+= bin32/$img.o\n";
-	print "IMGS32\t+= bin32/$img.img bin32/$img.lzimg bin32/$img.nrv2bimg\n"
+	print "DOBJS\t+= \$(BIN)/$img.o\n";
+	print "IMGS\t+= \$(BIN)/$img.img \$(BIN)/$img.lzimg \$(BIN)/$img.nrv2bimg\n"
 }
 
-print "ROMS32\t:=\n";
+print "ROMS\t:=\n";
 foreach my $family (sort keys %pcient) {
 	my $aref = $pcient{$family};
 	foreach my $entry (@$aref) {
 		my $rom = $entry->[0];
-		print "ROMS32\t+= bin32/$rom.rom bin32/$rom.lzrom bin32/$rom.nrv2brom\n";
+		print "ROMS\t+= \$(BIN)/$rom.rom \$(BIN)/$rom.lzrom \$(BIN)/$rom.nrv2brom\n";
 	}
 }
 foreach my $isa (sort keys %isaent) {
-	print "ROMS32\t+= bin32/$isa.rom bin32/$isa.lzrom bin32/$isa.nrv2brom\n";
+	print "ROMS\t+= \$(BIN)/$isa.rom \$(BIN)/$isa.lzrom \$(BIN)/$isa.nrv2brom\n";
 }
 
 # Generate the *.o rules
@@ -133,15 +144,16 @@ foreach my $pci (sort keys %drivers) {
 	# For ISA the rule for .o will generated later
 	next if &isaonly($pci);
 	# PCI drivers are compiled only once for all ROMs
-	(my $macro = $pci) =~ tr/\-/_/;
+	(my $macro = basename($pci)) =~ tr/\-/_/;
+	my $obj = basename($pci);
 	my $deps = $drivers{$pci};
 	print <<EOF;
 
-bin32/$pci.o:	$pci.c \$(MAKEDEPS) pci.h $deps
-	\$(CC32) \$(CFLAGS32) \$(\U$macro\EFLAGS) -o \$@ -c \$<
+\$(BIN)/$obj.o:	$pci.c \$(MAKEDEPS) include/pci.h $deps
+	\$(CC) \$(CFLAGS) \$(\U$macro\EFLAGS) -o \$@ -c \$<
 
-bin32/$pci--%.o:	bin32/%.o bin32/$pci.o \$(MAKEDEPS)
-	\$(LD32) -r bin32/$pci.o \$< -o \$@
+\$(BIN)/$obj--%.o:	\$(BIN)/%.o \$(BIN)/$obj.o \$(MAKEDEPS)
+	\$(LD) -r \$(BIN)/$obj.o \$< -o \$@
 
 EOF
 }
@@ -152,11 +164,11 @@ foreach my $isa (sort keys %isaent) {
 	my $base = $isaent{$isa};
 	my $deps = $drivers{$base};
 	print <<EOF;
-bin32/$isa.o:	$base.c \$(MAKEDEPS) $deps
-	\$(CC32) \$(CFLAGS32) \$(\U$macro\EFLAGS) -o \$@ -c \$<
+\$(BIN)/$isa.o:	$base.c \$(MAKEDEPS) $deps
+	\$(CC) \$(CFLAGS) \$(\U$macro\EFLAGS) -o \$@ -c \$<
 
-bin32/$isa--%.o:	bin32/%.o bin32/$isa.o \$(MAKEDEPS)
-	\$(LD32) -r bin32/$isa.o \$< -o \$@ 
+\$(BIN)/$isa--%.o:	\$(BIN)/%.o \$(BIN)/$isa.o \$(MAKEDEPS)
+	\$(LD) -r \$(BIN)/$isa.o \$< -o \$@ 
 EOF
 }
 
@@ -166,32 +178,33 @@ foreach my $family (sort keys %pcient) {
 	my $aref = $pcient{$family};
 	foreach my $entry (@$aref) {
 		my ($rom, $ids, $comment) = @$entry;
+		my $img = basename($family);
 		next if ($ids eq '-');
 		print <<EOF;
-bin32/$rom.rom:	bin32/$family.img \$(PRLOADER) \$(START16)
+\$(BIN)/$rom.rom:	\$(BIN)/$img.img \$(PRLOADER) \$(START16)
 	cat \$(PRLOADER) \$(START16) \$< > \$@
-	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT32) \$@
+	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT) \$@
 
-bin32/$rom--%.rom:	bin32/$family--%.img \$(PRLOADER) \$(START16)
+\$(BIN)/$rom--%.rom:	\$(BIN)/$img--%.img \$(PRLOADER) \$(START16)
 	cat \$(PRLOADER) \$(START16) \$< > \$@
-	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT32) \$@
+	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT) \$@
 
-bin32/$rom.lzrom:	bin32/$family.lzimg \$(PRLOADER) \$(START16)
+\$(BIN)/$rom.lzrom:	\$(BIN)/$img.lzimg \$(PRLOADER) \$(START16)
 	cat \$(PRLOADER) \$(START16) \$< > \$@
-	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT32) \$@
+	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT) \$@
 
-bin32/$rom--%.lzrom:	bin32/$family--%.huf \$(PRLOADER) \$(START16)
+\$(BIN)/$rom--%.lzrom:	\$(BIN)/$img--%.huf \$(PRLOADER) \$(START16)
 	cat \$(PRLOADER) \$(START16) \$< > \$@
-	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT32) \$@
+	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT) \$@
 
 
-bin32/$rom.nrv2brom:	bin32/$family.nrv2bimg \$(PRLOADER) \$(START16)
+\$(BIN)/$rom.nrv2brom:	\$(BIN)/$img.nrv2bimg \$(PRLOADER) \$(START16)
 	cat \$(PRLOADER) \$(START16) \$< > \$@
-	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT32) \$@
+	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT) \$@
 
-bin32/$rom--%.nrv2brom:	bin32/$family--%.nrv2bimg \$(PRLOADER) \$(START16)
+\$(BIN)/$rom--%.nrv2brom:	\$(BIN)/$img--%.nrv2bimg \$(PRLOADER) \$(START16)
 	cat \$(PRLOADER) \$(START16) \$< > \$@
-	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT32) \$@
+	\$(MAKEROM) \$(MAKEROM_FLAGS) \$(MAKEROM_\$*) -p $ids -i\$(IDENT) \$@
 
 EOF
 	}
@@ -200,9 +213,9 @@ EOF
 # ISA ROMs are prepared from the matching code images
 foreach my $isa (sort keys %isaent) {
 	print <<EOF;
-bin32/$isa.rom:		bin32/$isa.img \$(RLOADER)
+\$(BIN)/$isa.rom:		\$(BIN)/$isa.img \$(RLOADER)
 
-bin32/$isa.lzrom:	bin32/$isa.lzimg \$(RLOADER)
+\$(BIN)/$isa.lzrom:	\$(BIN)/$isa.lzimg \$(RLOADER)
 
 EOF
 }
