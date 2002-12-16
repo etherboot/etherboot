75b1a94832456b4953de388d4f4d678797644aec
Eric W. Biederman <ebiederman@lnxi.com>
2002-12-16 23:53:02 +0000

    - Second round of itanium specific files

---
+++ src/arch/ia64/include/bits/elf.h
@@ -0,0 +1,9 @@
+#ifndef IA64_BITS_ELF_H
+#define IA64_BITS_ELF_H
+
+/* ELF Defines for the current architecture */
+#define	EM_CURRENT	EM_IA_64
+#define ELFDATA_CURRENT	ELFDATA2LSB
+
+
+#endif /* IA64_BITS_ELF_H */
