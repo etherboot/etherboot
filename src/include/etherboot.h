6dd37ccaa3f707e95f7b69c48544f0ace1682d07
Michael Brown <mcb30@etherboot.org>
2003-10-20 12:16:17 +0000

    Because irq_handler will need to be derelocated, we can't simply declare irq_handler and irq_triggered as externs within etherboot.h. See pic8259.h for details.

--- src/include/etherboot.h
+++ src/include/etherboot.h
@@ -294,7 +294,6 @@ extern int int15 P((int));
 #ifdef	POWERSAVE
 extern void cpu_nap P((void));
 #endif	/* POWERSAVE */
-extern void irq_handler ( void );
 extern void fake_irq ( uint8_t irq );
 
 /* basemem.c */
@@ -422,9 +421,6 @@ extern char freebsd_kernel_env[FREEBSD_KERNEL_ENV_SIZE];
 /* bootmenu.c */
 
 /* osloader.c */
-
-/* pcbios.S */
-extern volatile uint16_t irq_triggered;
 
 /* created by linker */
 extern char _virt_start[], _text[], _etext[], _text16[], _etext16[];
