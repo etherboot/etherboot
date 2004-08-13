d86e98ca832665ea5aed7aa64b5a0bb1ae1710b5
Ken Yap <ken_yap@users.sourceforge.net>
2004-08-13 02:28:43 +0000

    Better way of preventing gcc from optimising away the static function.

--- src/arch/i386/core/pxe_callbacks.c
+++ src/arch/i386/core/pxe_callbacks.c
@@ -41,7 +41,8 @@
  * bug), ao we have to put these asm statements inside a dummy
  * function.
  */
-void work_around_gcc_bug ( void ) {
+static void work_around_gcc_bug ( void ) __attribute__ ((used));
+static void work_around_gcc_bug ( void ) {
 	/* Export sizeof(pxe_stack_t) as absolute linker symbol */
 	__asm__ ( ".globl _pxe_stack_t_size" );
 	__asm__ ( ".equ _pxe_stack_t_size, %c0"
