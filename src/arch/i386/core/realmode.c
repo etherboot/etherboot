e28d5d4506d5d217c93aa269d67a361935403a38
Ken Yap <ken_yap@users.sourceforge.net>
2004-08-13 02:42:02 +0000

    Prevent work_around_gcc_bug being optimised away.

--- src/arch/i386/core/realmode.c
+++ src/arch/i386/core/realmode.c
@@ -13,7 +13,7 @@
  * bug), ao we have to put these asm statements inside a dummy
  * function.
  */
-static void work_around_gcc_bug ( void ) __unused;
+static void work_around_gcc_bug ( void ) __attribute__ ((used));
 static void work_around_gcc_bug ( void ) {
 	/* Export _real_mode_stack_size as absolute linker symbol */
 	__asm__ ( ".globl _real_mode_stack_size" );
