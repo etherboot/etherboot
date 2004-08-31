4d87edba01ea1831c9dbe44f4de7679721fc1d91
Timothy Legge <tlegge@rogers.com>
2004-08-31 22:03:04 +0000

    Additional bug fixes for some chip revisions

--- src/drivers/net/sis900.c
+++ src/drivers/net/sis900.c
@@ -457,7 +457,7 @@ static u16 sis900_read_eeprom(int location)
 
     outl(0, ee_addr);
     eeprom_delay();
-    outl(EECLK, ee_addr);
+    outl(EECS, ee_addr);
     eeprom_delay();
 
     /* Shift the read command (9) bits out. */
@@ -468,7 +468,7 @@ static u16 sis900_read_eeprom(int location)
         outl(dataval | EECLK, ee_addr);
         eeprom_delay();
     }
-    outb(EECS, ee_addr);
+    outl(EECS, ee_addr);
     eeprom_delay();
 
     /* read the 16-bits data in */
@@ -484,7 +484,7 @@ static u16 sis900_read_eeprom(int location)
     /* Terminate the EEPROM access. */
     outl(0, ee_addr);
     eeprom_delay();
-    outl(EECLK, ee_addr);
+//  outl(EECLK, ee_addr);
 
     return (retval);
 }
@@ -648,7 +648,7 @@ sis900_reset(struct nic *nic __unused)
         status ^= (inl(isr + ioaddr) & status);
     }
 
-    if( (pci_revision == SIS635A_900_REV) || (pci_revision == SIS900B_900_REV) )
+    if( (pci_revision >= SIS635A_900_REV) || (pci_revision == SIS900B_900_REV) )
             outl(PESEL | RND_CNT, ioaddr + cfg);
     else
             outl(PESEL, ioaddr + cfg);
